f6a6d1a73855830b6c1fe5e38ea2c99991dfeb06
majdew <majd1999ziad@gmail.com>
2020-08-14 13:39:28 +0300

    use animated builder for signup animation

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -127,16 +127,13 @@ class _AuthCardState extends State<AuthCard>
         curve: Curves.linear,
       ),
     );
-    _heightAnimation.addListener(() {
-      setState(() {});
-    });
   }
 
   @override
   void dispose() {
+    super.dispose();
     _controller.dispose();
   }
-
 
   void _showErrorDialog(String message) {
     showDialog(
@@ -231,11 +228,15 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        height: _heightAnimation.value.height,
-        constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (context, child) => Container(
+          height: _heightAnimation.value.height,
+          constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0),
+          child: child,
+        ),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
